c15abb751579be5f29aebdeb54d962335edac5bf
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-09-22 05:20:17 +0200

    VacancyArAndroid: Add overlay

--- src/components/VacancyArAndroid.vue
+++ src/components/VacancyArAndroid.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="vacancy-ar-android">
     <img :src="vacancy.preview" />
-    (android detected)
+    <button @click="showOverlay = true">AR</button>
+    <div v-if="showOverlay" class="overlay">
+      <button @click="showOverlay = false">X</button>
+    </div>
   </div>
 </template>
 
@@ -10,5 +13,34 @@ export default {
   props: {
     vacancy: Object,
   },
+  data: () => ({
+    showOverlay: false,
+  }),
 };
 </script>
+
+<style lang="scss" scoped>
+.vacancy-ar-android {
+  position: relative;
+
+  img {
+    display: block;
+    width: 100%;
+  }
+
+  button {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+  }
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #fff;
+  }
+}
+</style>
